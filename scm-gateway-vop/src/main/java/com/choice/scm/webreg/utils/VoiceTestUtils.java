3546310b6db65849730092b901517ed5ceee2589
zhenjiaoming <zjm@choicesoft.com.cn>
2018-12-18 16:32:20 +0800

    commit:修改调用类

--- scm-gateway-vop/src/main/java/com/choice/scm/webreg/utils/VoiceTestUtils.java
+++ scm-gateway-vop/src/main/java/com/choice/scm/webreg/utils/VoiceTestUtils.java
@@ -36,36 +36,6 @@ public class VoiceTestUtils {
 
 
     /**
-     * 音频文件转化
-     * @param sourceFile
-     * @return
-     */
-    public byte[] audioToPcm(File sourceFile){
-        File targetFile=new File(ConvertParamConstants.TMP_FILE_PATH+sourceFile.getName()+".pcm");
-        log.info("create target file ,filename is {}",targetFile.getAbsolutePath());
-        String cmd=getConverCmd(sourceFile.getAbsolutePath(),targetFile.getAbsolutePath());
-        byte[] result=null;
-        try {
-            log.info("convert file cmd is {}",cmd);
-            ffmpeg.execute(cmd);
-            InputStream inputStream=new FileInputStream(targetFile);
-            result= IOUtils.toByteArray(inputStream);
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                FileUtils.forceDelete(sourceFile);
-                FileUtils.forceDelete(targetFile);
-            } catch (IOException e) {
-                e.printStackTrace();
-                log.error("delete tmp file error,msg={},errmsg={}",e.getMessage(),e.getStackTrace());
-            }
-        }
-        return result;
-    }
-
-    /**
      * 组装命令
      * @param sourcePath
      * @param targetPath
